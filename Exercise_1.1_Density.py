13ce64a4b7e92381aaba9543d3a1535ae708bddc
MrSpock1994 <willpokerpro2016@gmail.com>
2022-02-02 23:25:36 -0300

    "First commit"

---
+++ Exercise_1.1_Density.py
@@ -0,0 +1,8 @@
+print("Materials      | Polystyrene (low density)  Cork  Rhenium  Platinium")
+print("Density(kg/m³) | 20                         220   21020    21450")
+
+cm = 0.858
+cv = 0.00004
+
+d = cm / cv
+print(f"The density of the cube is {round(d, 2)}kg/m³")
